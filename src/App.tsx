2baa58a8f86299409487ce6b5067606cba1b5d48
André Ferreira <andreferreira2302145@gmail.com>
2022-11-10 19:11:18 +0000

    Navbar implementado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //livraria react-router-dom para implementar a navbar
 import './App.css';
+import {Navbar} from "./componentes/Navbar";
+import { Home } from "./paginas/Home";
+import { Menu } from "./paginas/Menu";
+import { Contact } from "./paginas/Contact";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1> Página não encontrada</h1>} />
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
---
+++ src/componentes/Navbar.tsx
@@ -0,0 +1,19 @@
+import {Link} from "react-router-dom"
+import './Navbar.css'
+
+export const Navbar = () =>{
+    return( 
+        <div>
+            <header>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1jtzmmE08TQLtQvkowcEmj6OJ6YAAu1zS4g&usqp=CAU"/>
+                <h1>Plantas Inventario</h1>
+                <div className="navbar">
+                    <Link to="/">Home </Link>
+                    <Link to="/menu">Menu </Link>
+                    <Link to="/contact">Contactos </Link>
+                </div>
+            </header>
+        </div>
+        
+    )
+}
